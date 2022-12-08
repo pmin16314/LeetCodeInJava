74865753dc5a7be378f5e45fd822fe7455ee6238
Panchana Madara <panchanamadara16314@gmail.com>
2022-12-08 13:34:52 +0530

    project 35

---
+++ src/Project35.java
@@ -0,0 +1,33 @@
+import javax.xml.transform.stax.StAXResult;
+
+public class Project35 {
+    public static int searchInsert(int[] nums, int target) {
+        int start =0;
+        int end = nums.length-1;
+
+        while (start<=end){
+            int mid = (start+end)/2;
+
+            if (nums[mid]==target){
+                return mid;
+            }
+
+            else if(nums[mid]<target){
+                start = mid+1;
+            }
+            else{
+                end=mid-1;
+            }
+        }
+        return end+1;
+    }
+
+
+    public static void main(String[] args) {
+        int[] x ={1,3,5,6};
+        int target = 10;
+
+        System.out.println(searchInsert(x, target));
+     }
+    
+}
